d735a54319bc1c35885fd3fb0ba04f90d0df9fcd
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-26 23:05:00 +0600

    Initial Commit

---
+++ AnimationSwiftUI/AnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSwiftUIApp.swift
+//  AnimationSwiftUI
+//
+//  Created by ikorobov on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
