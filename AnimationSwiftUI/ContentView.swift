50420cfbe6cbe410124f9f2e903da295290c5a35
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-27 18:38:56 +0600

    1

--- AnimationSwiftUI/ContentView.swift
+++ AnimationSwiftUI/ContentView.swift
@@ -8,15 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var wingsAngle = 50.0
+    var isFlewAway = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack() {
+                TriangleView(
+                    angle: -300,
+                    color: .yellow
+                )
+                .frame(width: 125, height: 125)
+                .offset(x: -110, y: 0)
+            
+                SemiCircleView(
+                    angle: wingsAngle,
+                    color: .blue
+                )
+                .frame(width: 150, height: 150)
+                .offset(x: 0, y: -90)
+                
+                TriangleView(
+                    angle: 0,
+                    color: .red
+                )
+                .frame(width: 50, height: 50)
+                .offset(x: -30 ,y: 40)
+                TriangleView(
+                    angle: 0,
+                    color: .red
+                )
+                .frame(width: 50, height: 50)
+                .offset(x: 0,y: 40)
+                SemiCircleView(
+                    angle: -30,
+                    color: .init(.systemGray)
+                )
+                .frame(width: 200, height: 200)
+                .offset(x: -40, y: -75)
+                
+                SemiCircleView(
+                    angle: wingsAngle,
+                    color: .cyan
+                )
+                .animation(Animation.easeInOut(duration: 1.0), value: 1)
+                .frame(width: 140, height: 140)
+                .offset(x: -20,y: -90)
+            
+            TriangleView(
+                angle: 100,
+                color: .red
+            )
+            .frame(width: 50, height: 50)
+            .offset(x: 130,y: -120)
+
+                Circle()
+                .foregroundColor(.yellow)
+                .frame(width: 100, height: 100)
+                .offset(x: 70,y: -130)
+            
+            Circle()
+                .frame(width: 10,height: 10)
+                .offset(x:90,y:-130)
+
         }
-        .padding()
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
