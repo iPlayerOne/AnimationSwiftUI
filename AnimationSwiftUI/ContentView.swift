9e78b5e80c4dd588c947291360f6ce34bcbcfde5
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-27 23:36:07 +0600

    centered a bit

--- AnimationSwiftUI/ContentView.swift
+++ AnimationSwiftUI/ContentView.swift
@@ -21,14 +21,14 @@ struct ContentView: View {
                     color: .yellow
                 )
                 .frame(width: 125, height: 125)
-                .offset(x: -110, y: 0)
+                .offset(x: -100, y: 0)
                 
                 SemiCircleView(
                     angle: isFlewAway ? -20 : 50,
                     color: .blue
                 )
                 .frame(width: 120, height: 120)
-                .offset(x: 0, y: -90)
+                .offset(x: 10, y: -90)
                 
                 
                 TriangleView(
@@ -36,38 +36,38 @@ struct ContentView: View {
                     color: .red
                 )
                 .frame(width: 50, height: 50)
-                .offset(x: -30 ,y: 40)
+                .offset(x: -20 ,y: 40)
                 TriangleView(
                     angle: 0,
                     color: .red
                 )
                 .frame(width: 50, height: 50)
-                .offset(x: 0,y: 40)
+                .offset(x: 10,y: 40)
                 
                 SemiCircleView(
                     angle: -30,
                     color: .init(.systemGray)
                 )
                 .frame(width: 200, height: 200)
-                .offset(x: -40, y: -75)
+                .offset(x: -30, y: -75)
                 
                 SemiCircleView(
                     angle: isFlewAway ? -20 : 50,
                     color: .cyan
                 )
                 .frame(width: 140, height: 140)
-                .offset(x: -20,y: -90)
+                .offset(x: -10,y: -90)
                 
                 TriangleView(
                     angle: 100,
                     color: .red
                 )
                 .frame(width: 50, height: 50)
-                .offset(x: 130,y: -120)
+                .offset(x: 140,y: -120)
                 
                 CircleView()
                     .frame(width: 100, height: 100)
-                    .offset(x: 70,y: -130)
+                    .offset(x: 80,y: -130)
                 
             }
             .animation(
@@ -80,7 +80,7 @@ struct ContentView: View {
                 Image(systemName: "globe")
                     .resizable()
                     .frame(width: 150, height: 150)
-                    .offset(x: -10, y: -30)
+                    .offset(x: 0, y: -30)
                     .foregroundColor(.accentColor)
                     .transition(.move(edge: .bottom))
             } else {
